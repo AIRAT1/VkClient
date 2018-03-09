7bc8a39e96e6d402875fee8884534a11c84f4d2c
Ayrat <ayrat1@mail.ru>
2018-03-09 16:35:59 +0300

    complete v5 06.24

--- app/src/main/java/de/android/ayrathairullin/vkclient/ui/fragment/NewsFeedFragment.java
+++ app/src/main/java/de/android/ayrathairullin/vkclient/ui/fragment/NewsFeedFragment.java
@@ -12,20 +12,14 @@ import javax.inject.Inject;
 import de.android.ayrathairullin.vkclient.MyApplication;
 import de.android.ayrathairullin.vkclient.R;
 import de.android.ayrathairullin.vkclient.common.utils.VkListHelper;
-import de.android.ayrathairullin.vkclient.model.WallItem;
 import de.android.ayrathairullin.vkclient.model.view.BaseViewModel;
 import de.android.ayrathairullin.vkclient.model.view.NewsItemBodyViewModel;
 import de.android.ayrathairullin.vkclient.model.view.NewsItemFooterViewModel;
 import de.android.ayrathairullin.vkclient.model.view.NewsItemHeaderViewModel;
 import de.android.ayrathairullin.vkclient.rest.api.WallApi;
 import de.android.ayrathairullin.vkclient.rest.model.request.WallGetRequestModel;
-import de.android.ayrathairullin.vkclient.rest.model.response.GetWallResponse;
 import io.reactivex.Observable;
-import io.reactivex.ObservableSource;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 public class NewsFeedFragment extends BaseFeedFragment {
@@ -47,31 +41,19 @@ public class NewsFeedFragment extends BaseFeedFragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        mWallApi.get(new WallGetRequestModel(-1090630).toMap()).flatMap(new Function<GetWallResponse, ObservableSource<WallItem>>() {
-            @Override
-            public ObservableSource<WallItem> apply(@NonNull GetWallResponse getWallResponse) throws Exception {
-                return Observable.fromIterable(VkListHelper.getWallList(getWallResponse.response));
-            }
-        })
-                .flatMap(new Function<WallItem, ObservableSource<BaseViewModel>>() {
-                    @Override
-                    public ObservableSource<BaseViewModel> apply(@NonNull WallItem wallItem) throws Exception {
-                        List<BaseViewModel> baseItems = new ArrayList<>();
-                        baseItems.add(new NewsItemHeaderViewModel(wallItem));
-                        baseItems.add(new NewsItemBodyViewModel(wallItem));
-                        baseItems.add(new NewsItemFooterViewModel(wallItem));
-                        return Observable.fromIterable(baseItems);
-                    }
+        mWallApi.get(new WallGetRequestModel(-1090630).toMap())
+                .flatMap(full -> Observable.fromIterable(VkListHelper.getWallList(full.response)))
+                .flatMap(wallItem -> {
+                    List<BaseViewModel> baseItems = new ArrayList<>();
+                    baseItems.add(new NewsItemHeaderViewModel(wallItem));
+                    baseItems.add(new NewsItemBodyViewModel(wallItem));
+                    baseItems.add(new NewsItemFooterViewModel(wallItem));
+                    return Observable.fromIterable(baseItems);
                 })
                 .toList()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<List<BaseViewModel>>() {
-                    @Override
-                    public void accept(List<BaseViewModel> objects) throws Exception {
-                        mAdapter.addItems(objects);
-                    }
-                });
+                .subscribe(objects -> mAdapter.addItems(objects));
     }
 
     @Override
