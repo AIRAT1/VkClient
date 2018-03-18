372b8cb668609f584fe4206de72e85c5e069c5cb
Ayrat <ayrat1@mail.ru>
2018-03-18 09:24:02 +0300

    v10 08.54

--- app/src/main/java/de/android/ayrathairullin/vkclient/ui/fragment/CommentsFragment.java
+++ app/src/main/java/de/android/ayrathairullin/vkclient/ui/fragment/CommentsFragment.java
@@ -1,6 +1,7 @@
 package de.android.ayrathairullin.vkclient.ui.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -13,6 +14,7 @@ import de.android.ayrathairullin.vkclient.R;
 import de.android.ayrathairullin.vkclient.model.Place;
 import de.android.ayrathairullin.vkclient.mvp.presenter.BaseFeedPresenter;
 import de.android.ayrathairullin.vkclient.mvp.presenter.CommentsPresenter;
+import de.android.ayrathairullin.vkclient.ui.activity.CreatePostActivity;
 //import io.reactivex.annotations.Nullable;
 
 public class CommentsFragment extends BaseFeedFragment {
@@ -40,7 +42,20 @@ public class CommentsFragment extends BaseFeedFragment {
         mPlace = new Place(getArguments());
     }
 
-
+    @Override
+    public void onResume() {
+        super.onResume();
+        getBaseActivity().mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseActivity(), CreatePostActivity.class);
+                intent.putExtra("type", "comment");
+                intent.putExtra("owner_id", Integer.parseInt(mPlace.getOwnerId()));
+                intent.putExtra("id", Integer.parseInt(mPlace.getPostId()));
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -65,5 +80,10 @@ public class CommentsFragment extends BaseFeedFragment {
     @Override
     public int onCreateToolbarTitle() {
         return R.string.screen_name_comments;
+    }
+
+    @Override
+    public boolean needFab() {
+        return true;
     }
 }
